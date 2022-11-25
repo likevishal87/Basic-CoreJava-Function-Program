fcc30524a6ca9c35f1d5cd7371754f3a3ba8df5a
Vicky Verma <likevishalverma@gmail.com>
2022-11-25 12:59:15 +0530

    The prime factors of number N

---
+++ Factors.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.day5;
+
+import java.util.Scanner;
+
+//Factors
+//a. Desc -> Computes the prime factorization of N using brute force.
+//b. I/P -> Number to find the prime factors
+//c. Logic -> Traverse till i*i <= N instead of i <= N for efficiency.
+//d. O/P -> Print the prime factors of number N.
+public class Factors {
+
+	public static void main(String[] args) {
+		int number;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a number ::");
+		number = scanner.nextInt();
+
+		for (int i = 2; i < number; i++) {
+			while (number % i == 0) {
+				System.out.println(i + " ");
+				number = number / i;
+			}
+		}
+		if (number > 2) {
+			System.out.println(number);
+		}
+
+	}
+
+}
